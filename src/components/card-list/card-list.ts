97ea966769bb73db9899547c40315bdf0e38fa35
Ryan <ryanjones@Ryans-MacBook-Pro.local>
2018-03-19 15:28:21 +0100

    fix: import and directive order in card-list

--- src/components/card-list/card-list.ts
+++ src/components/card-list/card-list.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { CardList, CardKpi } from '../../app/models/card-list.model';
 import { ModalController } from 'ionic-angular';
+import { CardList, CardKpi } from '../../app/models/card-list.model';
 import { PmcScorecardPage } from '../../pages/pmc/pmc-scorecard/pmc-scorecard';
 import { CardListService } from '../../services/card-list.service';
 
@@ -11,12 +11,9 @@ import { CardListService } from '../../services/card-list.service';
 export class CardListComponent {
   @Input() card: CardList;
 
-
   constructor(private modalCtrl: ModalController, private cardList: CardListService) {}
 
-  setArrowDirection(direction: string) {
-    return direction === 'down' ? 'md-arrow-dropdown' : 'md-arrow-dropup';
-  }
+  setArrowDirection = (direction: string) => direction === 'down' ? 'md-arrow-dropdown' : 'md-arrow-dropup';
 
   loadCardOverview(kpi: CardKpi) {
     const data = this.cardList.getKpiScoreCardData(kpi);
